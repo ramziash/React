35aa11ea0b366da902c138a1904ced3966bade42
Ramzi Alashabi <ramzialashabi1@gmail.com>
2020-05-13 09:03:40 +0200

    ammended

--- week1/react-exercises/src/components/e1.js
+++ week1/react-exercises/src/components/e1.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 
-const hobbyList = () => {
+const Hobby= ( {hobby} ) => {
+    return <li> {hobby} </li>
+} 
 
+
+const hobbyList = () => {
+   
     const hobbies = ["Surfing", "Rock climbing", "Mountain biking", "Breakdancing"];
-    
+
     return (
         <ul>
-            {hobbies.map(hobby => {
-                return <li>{hobby}</li>
-        })}
+            {hobbies.map(item => (
+                <Hobby hobby ={item}/>
+            ))}
         </ul>
     )
 }
+
+
+hobbyList()
+
 
 export default hobbyList;
